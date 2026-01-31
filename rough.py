79045a14c6ee6d1c3593d9f26f410f3daa5e9120
Alphacentauri05 <udaysharma051203@gmail.com>
2026-01-31 12:26:45 +0530

    rough.py added

--- rough.py
+++ rough.py
@@ -1,3 +1,7 @@
 #to run generate_embeddings.py 
 
-#python generate_embeddings.py E:\awaara_web_main\Event1 https://res.cloudinary.com/dthwmzrhz/image/upload
+#python generate_embeddings.py E:\awaara_web_main\Event1 https://res.cloudinary.com/dthwmzrhz/image/upload/events/ 
+
+
+
+#python upload_to_cloudinary.py ..\Event1(this is the folder from which the images have to be extracted)
